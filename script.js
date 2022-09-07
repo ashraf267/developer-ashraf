e3e6b2f8cb058d786e20f1d2f0af1d8482dc433e
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-07 12:10:48 +0100

    Added slideshows in script.js to my-slides. Fixed tiny bugs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// Typewriter effect
 var x = 0;
 var txt = "Software Developer";
-var speed = 105;
+var speed = 110;
 
 // Anonymous function
 (function typewriter() {
@@ -10,3 +11,21 @@ var speed = 105;
         setTimeout(typewriter, speed);
     }
 }) ()
+
+// Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let y;
+    let slides = document.getElementsByClassName("my-slides");
+    for (y = 0; y < slides.length; y++) {
+        slides[y].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2002); // 2002
+}
